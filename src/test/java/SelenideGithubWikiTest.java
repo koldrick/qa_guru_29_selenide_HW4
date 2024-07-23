4e6caebe42feca641d0938789f0671ee59667a27
Simon Kulakov <starckn1@gmail.com>
2024-07-23 22:04:27 +0300

    added SelenideGithubWikiTest

---
+++ src/test/java/SelenideGithubWikiTest.java
@@ -0,0 +1,41 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class SelenideGithubWikiTest {
+
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl= "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+
+    }
+
+    @Test
+    void pagesShouldHaveSoftAssertionsAndSoftAssertionsShouldHaveJUnit5Example(){
+        Selenide.open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".markdown-body").shouldHave(text("Soft assertions"));
+        $(byText("Soft assertions")).click();
+        $(".markdown-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                @Test
+                void test() {
+                Configuration.assertionMode = SOFT;
+                open("page.html");
+                $("#first").should(visible).click();
+                $("#second").should(visible).click();
+                """));
+    }
+
+
+}
